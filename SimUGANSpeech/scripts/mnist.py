581ad6bb64d3b62f7181ac39d303c7b4c3ad82ee
Allen Wang <allenwang28@yahoo.com>
2018-04-25 15:34:58 -0500

    Added comments to mnist script

--- SimUGANSpeech/scripts/mnist.py
+++ SimUGANSpeech/scripts/mnist.py
@@ -14,21 +14,29 @@ BATCH_SIZE = 100
 RESTORE = True
 
 if __name__ == "__main__":
+    # file paths
     mnist_save_path = os.path.join(DATA_DIR, 'mnist_ex')
     model_save_dir = os.path.join(TENSORFLOW_DIR, 'simple')
     model_save_path = os.path.join(model_save_dir, 'model.cpkt')
     mnist = input_data.read_data_sets(mnist_save_path, one_hot = True)
 
+    # specify classifier parameters
     input_shape = (None, 784)
     output_shape = (None, 10)
 
+    # construct classifier
     clf = SimpleNN(input_shape, output_shape, verbose=True)
 
+
+    # start tensorflow session
     sess = tensorflow.Session()
     sess.run(clf.initial_op())
 
+    # saver used for checkpoints
     saver = tensorflow.train.Saver()
 
+
+    # If we want to restore, restore if possible
     if RESTORE:
         print ("Restoring session")
         latest = tensorflow.train.latest_checkpoint(model_save_dir)
@@ -40,24 +48,29 @@ if __name__ == "__main__":
         print ("Not restoring session")
 
 
+    # Start training
     for i in range(NUM_ITER):
+        # Back up every 5 
         if i % 5 == 0:
             model_backup_save_path = os.path.join(model_save_dir, 'backup-{0}.cpkt'.format(i / 5))
+            saver.save(sess, model_backup_save_path)
 
+        # Test/Evaluate
         images, labels = mnist.test.images, mnist.test.labels
         error = sess.run(clf.error, {clf.input_tensor: images, clf.output_tensor: labels})
-
         print ('Test error: {:6.2f}%'.format(100 * error))
 
+        # Train
         for _ in range(100):
+            # Load batch
             batch_xs, batch_ys = mnist.train.next_batch(BATCH_SIZE)
             
+            # Create map for batch to graph
             feed_dict = { clf.input_tensor : batch_xs,
                           clf.output_tensor : batch_ys }
 
+            # Run a step of optimizer
             sess.run(clf.optimize, feed_dict=feed_dict)
-    
-        saver.save(sess, model_backup_save_path)
 
     save_path = saver.save(sess, model_save_path)
     print ("Saved session to {0}".format(save_path))
